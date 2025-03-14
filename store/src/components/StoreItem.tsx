99fcbd1af71b1e9dbdce9edaa8b0b6358951e8b4
keshav <keshavad80@gmail.com>
2025-03-14 11:29:04 +0545

    add

--- store/src/components/StoreItem.tsx
+++ store/src/components/StoreItem.tsx
@@ -31,10 +31,14 @@ type StoreItemProps = {
         style={{ objectFit: "cover"}}
          />
          <Card.Body className="d-flex flex-column">
-           <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
-            <span className="fs-2">{name}</span>
-            <span className="ms-2 text-muted">{formatCurrency(price)}</span>
-           </Card.Title> 
+           <Card.Title className="d-flex justify-content-between align-items-baseline ">
+             <span className="fs-6">{name}</span>
+             </Card.Title> 
+             <div className="d-flex justify-content-between align-items-center mb-4">
+            <span className=" text-muted fs-6">{formatCurrency(price)}</span>
+            
+           </div>
+
            <div className="mt-auto">
             {quantity === 0 ? (
                 <Button className="w-100" onClick={() => increaseCartQuantity(id)}>+ Add to cart</Button>
--- store/src/pages/Home.tsx
+++ store/src/pages/Home.tsx
@@ -1,3 +1,32 @@
+
+import { Container,Button, Row,Col } from "react-bootstrap";
+
 export function Home() {
-    return <h1> Home</h1>
+    return (
+   <div className="home-page">
+    <div className="hero-section">
+        <Container>
+        <Row className="align-items-center">
+                        <Col md={6}>
+                            <h6 className="text-danger">Dive in and Explore</h6>
+                            <h1 className="fw-bold">Start Shopping Now!</h1>
+                            <p>
+                                Explore our curated collections and find the perfect item that speaks
+                                to your style and needs. With just a click, begin your journey.
+                            </p>
+                            <Button variant="danger" className="mt-3">
+                                Shop Now
+                            </Button>
+                        </Col>
+                        <Col md={6}>
+                            <div className="hero-images">
+                                <img src="" alt="Hero 1"  />
+                                <img src="" alt="Hero 2"  />
+                            </div>
+                        </Col>
+                    </Row>
+        </Container>
+        </div>
+   </div>
+   );
 }
